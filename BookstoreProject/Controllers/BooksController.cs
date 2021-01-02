5454aca160a08658a73f462e8d61a7bbdf68f4cf
Burak Yazan <brkyznn@gmail.com>
2021-01-03 00:55:06 +0300

    kitaplar uzerinde tek gorsel guncelleme ozelligi eklendi, kitap gorsel formati icin hata mesaji eklendi.

--- BookstoreProject/Controllers/BooksController.cs
+++ BookstoreProject/Controllers/BooksController.cs
@@ -73,7 +73,7 @@ namespace BookstoreProject.Controllers
                     ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Name", book.AuthorId);
                     ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
                     ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", book.LanguageId);
-                    ViewData["ErrorMessage"] = _localizer["ErrorMessage1"];//"Fotoğraf eklemeden kitap yaratılamaz.";
+                    ViewData["ErrorMessage"] = _localizer["ErrorMessage1"];
                     return View();
                 }
 
@@ -157,7 +157,6 @@ namespace BookstoreProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Book book)
         {
-            //Book currentBook= _context.Books.Where(x => x.Id == id).FirstOrDefault();
             if (id != book.Id)
             {
                 return NotFound();
@@ -165,60 +164,60 @@ namespace BookstoreProject.Controllers
 
             if (ModelState.IsValid)
             {
-                if (book.MainPhotoFile == null || book.SecondPhotoFile == null || book.ThirdPhotoFile == null)
-                {
-                    ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Name", book.AuthorId);
-                    ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
-                    ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", book.LanguageId);
-                    ViewData["ErrorMessage"] = _localizer["ErrorMessage1"];
-                    return View();
-                }
-
                 string[] imgext = new string[3];
 
-                imgext[0] = Path.GetExtension(book.MainPhotoFile.FileName);
-                imgext[1] = Path.GetExtension(book.SecondPhotoFile.FileName);
-                imgext[2] = Path.GetExtension(book.ThirdPhotoFile.FileName);
+                imgext[0] = book.MainPhotoFile == null ? String.Empty : Path.GetExtension(book.MainPhotoFile.FileName);
+                imgext[1] = book.SecondPhotoFile == null ? String.Empty : Path.GetExtension(book.SecondPhotoFile.FileName);
+                imgext[2] = book.ThirdPhotoFile == null ? String.Empty : Path.GetExtension(book.ThirdPhotoFile.FileName);
 
-                if ((imgext[0] == ".jpg" || imgext[0] == ".png") && (imgext[1] == ".jpg" || imgext[1] == ".png") && (imgext[2] == ".jpg" || imgext[2] == ".png"))
+                if ((imgext[0] == ".jpg" || imgext[0] == ".png" || imgext[0] == String.Empty) && (imgext[1] == ".jpg" || imgext[1] == ".png" || imgext[1] == String.Empty) && (imgext[2] == ".jpg" || imgext[2] == ".png" || imgext[2] == String.Empty))
                 {
                     for (int i = 0; i < 3; i++)
                     {
-                        string path_name = Guid.NewGuid().ToString() + imgext[i];
-                        string saveimg = Path.Combine(_hostEnviroment.WebRootPath, "images", path_name);
-
-                        switch (i)
+                        if(imgext[i] != String.Empty)
                         {
-                            case 0:
-                                book.MainPhoto = path_name;
-                                using (var uploadimg = new FileStream(saveimg, FileMode.Create))
-                                {
-                                    await book.MainPhotoFile.CopyToAsync(uploadimg);
-                                }
-                                break;
-                            case 1:
-                                book.SecondPhoto = path_name;
-                                using (var uploadimg = new FileStream(saveimg, FileMode.Create))
-                                {
-                                    await book.SecondPhotoFile.CopyToAsync(uploadimg);
-                                }
-                                break;
-                            case 2:
-                                book.ThirdPhoto = path_name;
-                                using (var uploadimg = new FileStream(saveimg, FileMode.Create))
-                                {
-                                    await book.ThirdPhotoFile.CopyToAsync(uploadimg);
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                    }
+                            string path_name = Guid.NewGuid().ToString() + imgext[i];
+                            string saveimg = Path.Combine(_hostEnviroment.WebRootPath, "images", path_name);
+
+                            switch (i)
+                            {
+                                case 0:
+                                    book.MainPhoto = path_name;
+                                    using (var uploadimg = new FileStream(saveimg, FileMode.Create))
+                                    {
+                                        await book.MainPhotoFile.CopyToAsync(uploadimg);
+                                    }
+                                    break;
+                                case 1:
+                                    book.SecondPhoto = path_name;
+                                    using (var uploadimg = new FileStream(saveimg, FileMode.Create))
+                                    {
+                                        await book.SecondPhotoFile.CopyToAsync(uploadimg);
+                                    }
+                                    break;
+                                case 2:
+                                    book.ThirdPhoto = path_name;
+                                    using (var uploadimg = new FileStream(saveimg, FileMode.Create))
+                                    {
+                                        await book.ThirdPhotoFile.CopyToAsync(uploadimg);
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        } 
+                    } 
+                } else
+                {
+                    ViewData["AuthorId"] = new SelectList(_context.Authors.Where(a => a.Active == true), "Id", "Name", book.AuthorId);
+                    ViewData["CategoryId"] = new SelectList(_context.Categories.Where(a => a.Active == true), "Id", "Name", book.CategoryId);
+                    ViewData["LanguageId"] = new SelectList(_context.Languages.Where(a => a.Active == true), "Id", "Name", book.LanguageId);
+                    ViewData["ErrorMessage"] = _localizer["ErrorMessage2"];
+                    return View();
                 }
 
                 try
                 {
-                    
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
